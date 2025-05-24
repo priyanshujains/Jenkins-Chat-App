7c9f337f97753de8020706fa56ceac52a7686fe5
priyanshujains <jainpriyanshu0129@gmail.com>
2025-05-24 06:04:36 +0530

    updated files

--- front-chat/src/config/AxiosHelper.js
+++ front-chat/src/config/AxiosHelper.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseURL = "http://backend:8080";
+export const baseURL = "http://${window.location.hostname}:8080";
 export const httpClient = axios.create({
   baseURL: baseURL,
 });
